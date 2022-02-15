2dccac447b480fa5e03a7882fcd5ce10c60fbeaa
Tamago <yukotaniguchi@outlook.com>
2022-02-15 22:45:21 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+const list =[
+  {
+    title:'React',
+    url:'https://rectjs.org/',
+    author:'Jordan Wlake',
+    num_comments:3,
+    points: 4,
+    objectId:0,
+  },
+  {
+    title:'Redux',
+    url:'https://redux.js.org/',
+    author:'Dan Abramov, Andre Clark',
+    num_comments:2,
+    points: 5,
+    objectId:1,
+  }
+]
 
 function App() {
+  const title='React';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search</label>
+      <input id="search" typ="text" />
+
+      <hr />
+      {/* render the lsit here */}
+      <ul>
+        {list.map(function(item){
+          return (<li key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            </li>);
+        })}
+      </ul>
     </div>
   );
 }
